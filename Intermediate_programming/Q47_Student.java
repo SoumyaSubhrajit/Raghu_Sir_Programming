0c2514fcdd625e62a59a9f7cf0cace98b365c49a
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-10-20 20:25:07 +0530

    Add files via upload

---
+++ Intermediate_programming/Q47_Student.java
@@ -0,0 +1,12 @@
+public abstract class Q47_Student {
+    String name;
+    int id;
+    String stream;
+   public  void properties(String name, int id , String stream )
+   {
+       String na;
+       int i ;
+       String st;
+   }
+
+}
