0eadf8214dc07025188b677eff39f95b0424bd72
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-10-23 02:27:58 +0530

    Add files via upload

---
+++ Intermediate_programming/Q50_paralleogram_using_static_intialization_block.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q50_paralleogram_using_static_intialization_block {
+    static {
+        double height = 15.3;
+        double breath = 40.565;
+        double res = height * breath;
+        System.out.println("The Area of the parallogram is:"+" "+res);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
