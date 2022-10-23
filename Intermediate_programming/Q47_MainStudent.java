6fcafe79739ea9d11af2c1bc7d7c2699ebbbc343
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-10-24 00:25:52 +0530

    Add files via upload

---
+++ Intermediate_programming/Q47_MainStudent.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q47_MainStudent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name, id , stream");
+        int id = sc.nextInt();
+        sc.nextLine();
+        String name = sc.nextLine();
+        String stream = sc.next();
+      /*  Q47_Student std = new Q47_Student(id , stream , name);*/
+        System.out.println();
+    }
+
+}
