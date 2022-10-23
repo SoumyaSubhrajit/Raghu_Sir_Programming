311636a43d26f4d026473d9b0c191b3ed068fe74
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-10-24 00:28:51 +0530

    Add files via upload

---
+++ Intermediate_programming/Q51_deafulVaue_of_each_Datatype.java
@@ -0,0 +1,11 @@
+public class Q51_deafulVaue_of_each_Datatype {
+   static int i;
+    static String k;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+        System.out.println(k);
+
+
+    }
+}
