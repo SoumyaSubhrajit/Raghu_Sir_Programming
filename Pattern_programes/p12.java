72bb3a9cc4f7889f83150d0cd6d3fe87aed2d77e
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-10-26 01:34:20 +0530

    Add files via upload

---
+++ Pattern_programes/p12.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class p12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        for(int i = n; i>=1; i--)
+        {
+            for(int j=i;j>=1; j--)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
