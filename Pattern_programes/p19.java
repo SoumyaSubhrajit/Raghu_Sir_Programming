13ae285d0677d66df42c7360c1a45d95cabc8a69
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-10-28 02:10:34 +0530

    Add files via upload

---
+++ Pattern_programes/p19.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class p19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(i ==1 || j==1 || i == n || j==n)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
