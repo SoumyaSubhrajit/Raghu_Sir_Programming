02147ffb7bbe7fc498a3e33d4db9fefa3e064681
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-10-30 17:28:15 +0530

    Add files via upload

---
+++ Pattern_programes/p24.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class p24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        int x =1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                if(x==10){
+                    x= 1;
+                }
+                System.out.print(x+" ");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
