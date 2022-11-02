9d92a07bf10e3de7f70aaf97c869d26babb56f4d
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-11-03 03:54:04 +0530

    Add files via upload

---
+++ Pattern_programes/p32.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class p32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        int sp =1;
+        int st =n;
+        for(int i=1 ; i<=n; i++)
+        {
+            for(int j=1; j<=sp;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1 ; j<=st; j++)
+            {
+                System.out.print("* ");
+            }
+            if(i<=n/2)
+            {
+                sp++;
+                st = st-2;
+            }
+            else{
+                 sp--;
+              st = st+2;    }
+            System.out.println();
+        }
+    }
+}
