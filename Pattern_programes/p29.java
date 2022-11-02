d50458846db86df3a8b5e7061a865f992d25fd25
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-11-02 21:50:52 +0530

    Add files via upload

---
+++ Pattern_programes/p29.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class p29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        int sp=n/2;
+        int st=1;
+        for(int i=1 ;i<=n; i++)
+        {
+            for(int j=1; j<=sp; j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j =1; j<=st; j++)
+            {
+                if(j==1 || j==st){
+                System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+
+
+            if(i<=n/2){
+                sp--;
+                st=st+2;
+            }
+            else{
+                sp++;
+                st =st-2;
+            }
+            System.out.println();
+        }
+
+    }
+}
