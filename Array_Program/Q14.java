71527c2367826877d7cf7298a58432380135035d
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2022-11-19 09:45:55 +0530

    Add files via upload

---
+++ Array_Program/Q14.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Q14 {
+    public  int[] toRead() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a size of an array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the" + " " + n + " integer value");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+    public int getPrime(int[] arr)
+    {
+        int count=0;
+        for(int i=0; i< arr.length; i++)
+        {
+            if(arr[i]<1000) {
+                int fac = 0;
+                for (int j = 2; j * j <= arr[i]; j++) {
+                    if (arr[i] % j == 0) {
+                        fac++;
+                    }
+                }
+
+                if (fac == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Q14 q =  new Q14();
+        int[] arr = q.toRead();
+        int prime = q.getPrime(arr);
+        System.out.println("The number of prime number are- "+prime);
+    }
+}
